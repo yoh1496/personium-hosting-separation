11351da62e7c26faf69267486e67a353467e41eb
ishiguro-yo <yoh1496@gmail.com>
2020-07-31 18:40:48 +0900

    set config

---
+++ src/app/frontend/Constants/AppConstant.js
@@ -0,0 +1,5 @@
+export const AppConstant = {
+  cellUrl: 'https://app-ishiguro-02.appdev.personium.io/',
+  installBoxName: 'app-ishiguro-02',
+  barFileUrl: 'https://app-ishiguro-02.appdev.personium.io/app-ishiguro-02.bar',
+};
